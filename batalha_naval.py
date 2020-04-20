d0181811d2947f9bf939630c1096db2ca7868734
Lucas Henrique de Abreu <lucas.abreu@softfocus.com.br>
2020-04-20 20:29:09 -0300

    correçoes

--- batalha_naval.py
+++ batalha_naval.py
@@ -6,7 +6,12 @@ from random import randint
 from time import sleep
 import sys
 
+jogo_acabou = False
+vez = 'usuario'
+pontos_usuario = 0
+pontos_computador = 0
 tabuleiro = []
+
 #inicializa o tabuleiro
 for x in range(0, 8):
     linha = []
@@ -17,8 +22,8 @@ for x in range(0, 8):
 
 def mostrar_tabuleiro():
     print('\033[34m=-'*20)
-    print('\033[31m Máquina')
-    print('\033[32m Você')
+    print('\033[31m Pontos do computador: {}'.format(pontos_computador))
+    print('\033[32m Seus pontos: {}'.format(pontos_usuario))
     print('\033[34m=-'*20)
     print('    \033[34m0    1    2    3    4    5    6    7\033[m')
     for i, linha in enumerate(tabuleiro):
@@ -102,41 +107,70 @@ print('2º rodada, BATALHA!.')
 print('\033[34m=-'*20, end='\033[m')
 print()
 
-jogo_acabou = False
-vez = 'usuario'
-pontos_usuario = 0
-pontos_computador = 0
-
 while not jogo_acabou:
     if vez == 'usuario':
         #perdir para o usuário onde ele quer mandar o míssil
         while True:
             linha_coluna = input('Informe linha e coluna na ordem respectiva: ')
             if len(linha_coluna) == 2:
-                linha = int(linha_coluna[0])
-                coluna = int(linha_coluna[1])
-                if linha < 4:
-                    if coluna > 7:
-                        print('Você está jogando o míssil muito longe, vai para fora do tabuleiro. hehhehh')
-                        print('Tente novamente')
-                    else:
-                        if tabuleiro[linha][coluna] == '\033[33m╧\033[m':
-                            pontos_usuario += 1
-                            tabuleiro[linha][coluna] = '\033[37m☠\033[m'
-                            print('Vocế destruiu um barco do inimigo')
-                            mostrar_tabuleiro()
-                            break
-                        else:
-                            tabuleiro[linha][coluna] = '\033[34m♒\033[m'
-                            print('Infelizmente você errou!')
-                            mostrar_tabuleiro()
+                linha = linha_coluna[0]
+                coluna = linha_coluna[1]
+                if not linha.isnumeric() or not coluna.isnumeric():
+                    print('\033[31mÔOooo, apedeuta! Linhas e colunas só podem ser números.\033[m')
                 else:
-                    print('Você está tentando jogar um míssil no seu campo de batalha')
-                    print('Tente novamente')
+                    linha = int(linha)
+                    coluna = int(coluna)
+                    if linha < 4:
+                        if coluna > 7:
+                            print('Você está jogando o míssil muito longe, vai para fora do tabuleiro. hehhehh')
+                            print('Tente novamente')
+                        elif tabuleiro[linha][coluna] == '\033[34m*\033[m' or tabuleiro[linha][coluna] == '\033[37m☠\033[m':
+                            print('Você já jogou uma bomba nesta posição, não disperdice munição!')
+                        else:
+                            if tabuleiro[linha][coluna] == '\033[33m╧\033[m':
+                                pontos_usuario += 1
+                                tabuleiro[linha][coluna] = '\033[37m☠\033[m'
+                                print('Vocế destruiu um barco do inimigo')
+                            else:
+                                tabuleiro[linha][coluna] = '\033[34m*\033[m'
+                                print('Infelizmente você errou!')
+                            
+                            break
+                    else:
+                        print('Você está tentando jogar um míssil no seu campo de batalha')
+                        print('Tente novamente')
             else:
                 print('Você precisa informar linha e coluna, respectivamente')
                 print('Tente novamente')
+            
+        mostrar_tabuleiro()
+        vez = 'computador'
+        if pontos_usuario == 3:
+            jogo_acabou = True
+            print('Jogo acabou, usuário é vencedor!')
+            
     else:
-        print('O computador joga')
+        print('O computador jogando!')
+        carregando()
+        
+        while True:
+            coluna = randint(0, 7)
+            linha = randint(4, 7)
+            if tabuleiro[linha][coluna] != '\033[34m*\033[m' and tabuleiro[linha][coluna] != '\033[37m☠\033[m':
+                break
+
+        if tabuleiro[linha][coluna] == '\033[33m╧\033[m':
+            pontos_computador += 1
+            tabuleiro[linha][coluna] = '\033[37m☠\033[m'
+            print('Computador destruiu um de seus barcos')
+        else:
+            tabuleiro[linha][coluna] = '\033[34m*\033[m'
+            print('Computador errou!')
+
+        mostrar_tabuleiro()
+        vez = 'usuario'
+        if pontos_computador == 3:
+            jogo_acabou = True
+            print('Jogo acabou, computador é vencedor!')
 
     
